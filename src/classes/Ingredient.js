5f94f564aa4c889a2480c7c04e270e04e81e2cf9
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-08-30 20:50:04 -0600

    Edit Ingredient class using updated test suite

--- src/classes/Ingredient.js
+++ src/classes/Ingredient.js
@@ -1,12 +1,22 @@
+import { ingredientsData } from '../src/data/ingredients'
+
 class Ingredient {
   constructor(ingredient) {
     this.id = ingredient.id;
-    this.name = ingredient.name;
-    this.estimatedCostInCents = ingredient.estimatedCostInCents;
+    this.amount = ingredient.quantity.amount;
+    this.unit = ingredient.quantity.unit;
+    this.name = retrieveData('name');
+    this.estimatedCostInCents = retrieveData('estimatedCostInCents');
+    this.totalCostInDollars = calculateCost();
   }
 
-  calculateCost(amount) {
-    return this.estimatedCostInCents * amount / 100;
+  retrieveData(property) {
+    const ingredient = ingredientsData.find(ingredient => ingredient.id === this.id);
+    this[property] = ingredient[property];
+  }
+
+  calculateCost() {
+    return this.estimatedCostInCents * this.amount / 100;
   }
 }
 
