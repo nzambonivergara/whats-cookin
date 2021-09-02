91e95373bbc97b87b5e70fe74f253cb0d0eae6a1
Paul Chambers <paultimothychambers@gmail.com>
2021-09-01 21:35:49 -0600

    Refactor

--- test/User-test.js
+++ test/User-test.js
@@ -6,27 +6,23 @@ import { recipeData } from '../src/data/recipes-sample.js';
 import { ingredientsData } from '../src/data/ingredients-sample.js';
 
 describe('User', function() {
-  let user;
-  let recipeOne;
-  let recipeTwo;
-  let recipeThree;
-  let tagOne;
-  let tagTwo;
+  let user, recipeOne, recipeTwo, recipeThree, tagOne, tagTwo, tagThree, ingredientOne, nameOne;
 
-  beforeEach(() => {
+  beforeEach(function() {
     user = new User(usersData[0]);
     recipeOne = new Recipe(recipeData[2]);
     recipeTwo = new Recipe(recipeData[1]);
     recipeThree = new Recipe(recipeData[0]);
     tagOne = 'appetizer'
     tagTwo = 'snack'
+    tagThree = 'side dish'
+    nameOne = 'cookies'
     user.addFavorite(recipeOne);
     user.addFavorite(recipeTwo);
     user.addFavorite(recipeThree);
     user.addWeeklyRecipe(recipeOne);
     user.addWeeklyRecipe(recipeTwo);
     user.addWeeklyRecipe(recipeThree);
-
   });
 
   it('should have a name', function() {
@@ -53,41 +49,37 @@ describe('User', function() {
   });
 
   it('should be able to filter favorites by recipe tag', function() {
-    user.filterFavoriteRecipesByTags(tagOne);
-    expect(user.filteredFavoritesByTag).to.deep.equal(
+    expect(user.filterFavoriteRecipesByTags([tagOne])).to.deep.equal(
       [recipeOne, recipeThree]
     );
   });
 
   it('should be able to filter favorites by more than one recipe tag',
     function() {
-      user.filterFavoriteRecipesByTags(tagOne, tagTwo);
-      expect(user.filteredFavoritesByTag).to.deep.equal(
-        [recipeOne, recipeThree]
+      expect(user.filterFavoriteRecipesByTags([tagOne, tagTwo, tagThree])).to.deep.equal(
+        [recipeOne, recipeTwo, recipeThree]
       );
     });
 
-  it('should be able to filter favorites by ingredient', function() {
-    user.filterFavoriteRecipesByIngredient('wheat flour');
-    expect(user.filteredByIngredient).to.deep.equal([recipeOne, recipeThree]);
+  it.only('should be able to filter favorites by ingredient', function() {
+    expect(user.filterFavoriteRecipesByIngredient(['wheat flour'], ingredientsData)).to.deep.equal([recipeOne, recipeThree]);
   });
 
-  it('should be able to filter favorites by name', function() {
-    // the test portion commented out is the less spicy version; the test not commented out is SUPER spicy, ngl
-    user.filterFavoriteRecipesByName('cookies'); //or => ('Loaded Chocolate Chip Pudding Cookie Cups');
-    expect(user.filteredByName).to.deep.equal([recipeOne, recipeThree]); //or => ([recipeOne]);
+  it.skip('should be able to filter favorites by name', function() {
+    user.filterFavoriteRecipesByName([nameOne]);
+    expect(user.filteredByName).to.deep.equal([recipeOne, recipeThree]);
   });
 
-  it('should be able to add a recipe to the user\'s weekly meal plan',
+  it.skip('should be able to add a recipe to the user\'s weekly meal plan',
     function() {
       expect(user.weeklyFavorites).to.deep.equal(
         [recipeOne, recipeTwo, recipeThree]
       );
     });
 
-  it('should be able to remove a recipe from the user\'s weekly meal plan',
+  it.skip('should be able to remove a recipe from the user\'s weekly meal plan',
     function() {
       user.removeWeeklyRecipe(recipeTwo);
-      expect(user.favoriteRecipes).to.deep.equal([recipeOne, recipeThree]);
+      expect(user.weeklyFavorites).to.deep.equal([recipeOne, recipeThree]);
     });
 })
