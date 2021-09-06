a10c6ddef984b6c35403c90c959756b48ecccf97
Paul Chambers <paultimothychambers@gmail.com>
2021-09-05 20:15:07 -0600

    Add fetch recipes data function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -12,7 +12,15 @@ function loadIngredients() {
     .catch(error => console.error(error))
 }
 
+function loadRecipes() {
+  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+    .then(response => response.json())
+    .then(data => data.recipeData)
+    .catch(error => console.log(error))
+}
+
 export {
   loadUsers,
   loadIngredients,
+  loadRecipes
 }
