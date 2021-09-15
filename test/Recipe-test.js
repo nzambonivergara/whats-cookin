ec367adff509aedbe41ea3994e9fd938a5edcb41
Paul Chambers <paultimothychambers@gmail.com>
2021-09-15 17:04:57 -0600

    Remove hardcoded data, import from sample file

--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,64 +1,14 @@
 import { expect } from 'chai';
 import Ingredient from '../src/classes/Ingredient';
 import Recipe from '../src/classes/Recipe';
-import { ingredientsData } from '../src/data/ingredients-sample'
+import { ingredientsData } from '../src/data/ingredients-sample';
+import { recipeData } from '../src/data/recipes-sample';
 
 describe('Recipe Class', function() {
-  let recipeDetails, recipe;
+  let recipe;
 
   beforeEach(function() {
-    recipeDetails = {
-      "id": 595736,
-      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-      "ingredients": [
-        {
-          "id": 20081,
-          "quantity": {
-            "amount": 1.5,
-            "unit": "c"
-          }
-        },
-        {
-          "id": 18372,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "tsp"
-          }
-        },
-        {
-          "id": 1123,
-          "quantity": {
-            "amount": 1,
-            "unit": "large"
-          }
-        }
-      ],
-      "instructions": [
-        {
-          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
-          "number": 1
-        },
-        {
-          "instruction": "Add egg and vanilla and mix until combined.",
-          "number": 2
-        },
-        {
-          "instruction": "Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.",
-          "number": 3
-        }
-      ],
-      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
-      "tags": [
-        "antipasti",
-        "starter",
-        "snack",
-        "appetizer",
-        "antipasto",
-        "hor d'oeuvre"
-      ]
-    };
-
-    recipe = new Recipe(recipeDetails);
+    recipe = new Recipe(recipeData[3]);
   });
 
   it('should be a function', function() {
@@ -70,6 +20,7 @@ describe('Recipe Class', function() {
   });
 
   describe('Recipe constructor properties', function() {
+
     it('should have an id', function() {
       expect(recipe.id).to.equal(595736);
     });
@@ -79,7 +30,7 @@ describe('Recipe Class', function() {
     });
 
     it('should store ingredients', function() {
-      expect(recipe.ingredients).to.equal(recipeDetails.ingredients);
+      expect(recipe.ingredients).to.equal(recipeData[3].ingredients);
     });
 
     it('should be able to create instances of Ingredients and store them', function() {
@@ -94,7 +45,7 @@ describe('Recipe Class', function() {
     it('should store a list of instructions', function() {
       expect(recipe.instructions).to.be.an('array');
       expect(recipe.instructions[0].number).to.equal(1);
-      expect(recipe.instructions).to.deep.equal(recipeDetails.instructions);
+      expect(recipe.instructions).to.deep.equal(recipeData[3].instructions);
     });
 
     it('should have a name', function() {
@@ -104,11 +55,12 @@ describe('Recipe Class', function() {
     it('should store a list of tags', function() {
       expect(recipe.tags).to.be.an('array');
       expect(recipe.tags[0]).to.be.a('string');
-      expect(recipe.tags).to.equal(recipeDetails.tags);
+      expect(recipe.tags).to.equal(recipeData[3].tags);
     });
   });
 
   describe('Recipe functionality', function() {
+
     it('should return a list of ingredients', function() {
       recipe.getIngredientsInformation(ingredientsData);
       const listOfIngredients = recipe.returnIngredientsList();
